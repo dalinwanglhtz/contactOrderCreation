4a25e2184fd80e40025713a3e9b939e03f4c41a4
dalinwanglhtz <dalinwanglhtz@gmail.com>
2021-05-05 23:48:05 +1000

    add navigation to record page once record saved in lwc component

--- force-app/main/default/lwc/contactCreate/contactCreate.js
+++ force-app/main/default/lwc/contactCreate/contactCreate.js
@@ -1,5 +1,6 @@
 import { LightningElement, api } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { NavigationMixin } from 'lightning/navigation';
 
 import FIRSTNAME_FIELD from '@salesforce/schema/Contact.FirstName';
 import LASTNAME_FIELD from '@salesforce/schema/Contact.LastName';
@@ -8,8 +9,8 @@ import EMAIL_FIELD from '@salesforce/schema/Contact.Email';
 import MOBILEPHONE_FIELD from '@salesforce/schema/Contact.MobilePhone';
 import CONTACTROLE_FIELD from '@salesforce/schema/Contact.Contact_Role__c';
 
-export default class ContactCreate extends LightningElement {
-    @api recordId;
+export default class ContactCreate extends NavigationMixin(LightningElement) {
+    recordId;
 
     firstName = FIRSTNAME_FIELD;
     lastName = LASTNAME_FIELD;
@@ -19,11 +20,24 @@ export default class ContactCreate extends LightningElement {
     contactRole = CONTACTROLE_FIELD;
 
     handleSuccess(event) {
+        this.recordId = event.detail.id;
         const evt = new ShowToastEvent({
             title: "Contact Created",
             message: "Record ID: "+event.detail.id,
             variant: "success"
         });
         this.dispatchEvent(evt);
+        this.navigateToRecordPage();
+    }
+
+    navigateToRecordPage() {
+        this[NavigationMixin.Navigate]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: this.recordId,
+                objectApiName: 'Contact',
+                actionName: 'view'
+            }
+        });
     }
 }
--- force-app/main/default/lwc/orderCreate/orderCreate.js
+++ force-app/main/default/lwc/orderCreate/orderCreate.js
@@ -1,23 +1,39 @@
 import { LightningElement } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { NavigationMixin } from 'lightning/navigation';
 
 import ORDERSTARTDATE_FIELD from '@salesforce/schema/Order.EffectiveDate';
 import ACCOUNTNAME_FIELD from '@salesforce/schema/Order.AccountId';
 import STATUS_FIELD from '@salesforce/schema/Order.Status';
 import CONTACT1_FIELD from '@salesforce/schema/Order.CustomerAuthorizedById';
 
-export default class OrderCreate extends LightningElement {
+export default class OrderCreate extends NavigationMixin(LightningElement) {
+    recordId;
+
     orderStartDate = ORDERSTARTDATE_FIELD;
     accountId = ACCOUNTNAME_FIELD;
     status = STATUS_FIELD;
     customerAuthorizedById = CONTACT1_FIELD;
 
     handleSuccess(event) {
+        this.recordId = event.detail.id;
         const evt = new ShowToastEvent({
             title: "Order Created",
             message: "Record ID: "+event.detail.id,
             variant: "success"
         });
         this.dispatchEvent(evt);
+        this.navigateToRecordPage();
+    }
+
+    navigateToRecordPage() {
+        this[NavigationMixin.Navigate]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: this.recordId,
+                objectApiName: 'Order',
+                actionName: 'view'
+            }
+        });
     }
 }
